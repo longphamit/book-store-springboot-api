e7b4bdef8f7485dd92e3093970147beb2078a928
longphamit <longphamjustdoit@gmail.com>
2021-01-26 11:16:41 +0700

    first commit

---
+++ src/main/java/com/web/BookStoreSpringbootApiApplication.java
@@ -0,0 +1,13 @@
+package com.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreSpringbootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreSpringbootApiApplication.class, args);
+	}
+
+}
